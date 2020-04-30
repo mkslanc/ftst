9b5750905064b7e1c31c5a50ab79df27a02d1066
mkslanc <mks-tech@ya.ru>
2020-04-30 23:04:59 +0300

    more precise diffs

--- diff.js
+++ diff.js
@@ -2,7 +2,7 @@ require('colors');
 var jsdiff = require('diff');
 var fs = require("fs");
 const Path = require('path');
-
+var hrstart = process.hrtime();
 function generateJavaScriptFile(path, options) {
     deleteFolderRecursive(process.argv[3]);
     add(path[0]);
@@ -22,12 +22,12 @@ function generateJavaScriptFile(path, options) {
             //TODO: delete it! for our tests we just ignoring files with "static" word
             if (first && second && !/static\s/.test(first)) {
                 var span = '';
-                first = first.replace(/[/][*][\s\S]*?[*][/]/gm, "");
+                first = first.replace(/(?<=^|[^/]|[*][/])[/][*][\s\S]*?[*][/]/gm, "");
                 //first = first.replace(/\/\/\s@Filename: .*?\.json.*?(?=\/\/\s@|$)/gs, "");
                 first = first.replace(/[/][/].*$/gm, "");
                 var moduleMatch = first.match(/module[.]exports\s*=.*(?=$|\s*[/][/])/m);
                 if (moduleMatch) {
-                    var moduleFoundRegExp = new RegExp(moduleMatch[0].replace(/([()])/g,"\\$1"));
+                    var moduleFoundRegExp = new RegExp(moduleMatch[0].replace(/([()\[\]])/g,"\\$1"));
                     if (moduleFoundRegExp.test(second)) {
                         first = first.replace(moduleFoundRegExp, "");
                         second = second.replace(moduleFoundRegExp, "");
@@ -41,22 +41,41 @@ function generateJavaScriptFile(path, options) {
                         second = second.replace(defaultFoundRegExp, "");
                     }
                 }
-                second = second.replace(/[/][*][\s\S]*?[*][/]/gm, "");
+                var exportMatch = first.match(/exports[.][\w_]+\s*=\s*[\w._]+/gm);
+                if (exportMatch) {
+                    for (var i = 0; i < exportMatch.length; i++) {
+                        var exportFoundRegExp = new RegExp(exportMatch[i]);
+                        if (exportFoundRegExp.test(second)) {
+                            first = first.replace(exportFoundRegExp, "");
+                            second = second.replace(exportFoundRegExp, "");
+                        }
+                    }
+                }
+                second = second.replace(/(?<=^|[^/])[/][*][\s\S]*?[*][/]/gm, "");
                 //second = second.replace(/\/\/\s@Filename: .*?\.json.*?(?=\/\/\s@|$)/gs, "");
                 second = second.replace(/[/][/].*$/gm, "");
+                //TODO: experimental!
+                first = first.replace(/constructor\((?:[\w\s,]*)?\)\s*{/gm, "");
+                second = second.replace(/constructor\((?:[\w\s,]*)?\)\s*{/gm, "");
+                first = first.replace(/(?:\(\s*)?this(?:\s*\))?\./gm, "");
+                second = second.replace(/(?:\(\s*)?this(?:\s*\))?\./gm, "");
+                first = first.replace(/super\(.*?\)/gs, "");
+                second = second.replace(/super\(.*?\)/gs, "");
+                // ^^^^^^^^^^^^^^^^^^^^^^^^^
                 first = first.replace(/(\s)+/gs,'$1');
                 second = second.replace(/(\s)+/gs,'$1');
                 first = first.replace(/[;]+/gs,' ');
                 second = second.replace(/[;]+/gs,' ');
+
                 var diff = jsdiff.diffWords(first, second);
                 if (diff.length > 1 || (diff.length == 1 && (diff[0].added || diff[0].removed))) {
                     for (var i = 0; i < diff.length; i++) {
                         if ((diff[i].removed || diff[i].added)) {
-                            if (/^[*\s;'"(),/-~!]+$/s.test(diff[i].value)) {
+                            if (/^[*\s;'"(),/~.-]+$/s.test(diff[i].value)) {
                                 diff.splice(i, 1);
                                 i--;
                             } else {
-                                if ((/^constructor\((?:[\w,]*)?\)\s*{\s*(?:super\(...arguments\)[; ])?\s*(:?this\.)?$/s.test(diff[i].value) || /^this\.$/s.test(diff[i].value) || /^\s*[{}];?\s*$/s.test(diff[i].value))) {
+                                if ((/^(?:constructor\((?:[\s\w,]*)?\)\s*)?(?:{\s*)?(?:super\(...arguments\)[; ])?\s*(:?this\.)?$/s.test(diff[i].value) || /^this\.$/s.test(diff[i].value) || /^\s*[{}];?\s*$/s.test(diff[i].value))) {
                                     diff.splice(i, 1);
                                     i--;
                                 } else {
@@ -68,6 +87,11 @@ function generateJavaScriptFile(path, options) {
                                         if (/"__esModule", { value: true }\)[; ]/.test(diff[i].value)) {
                                             diff.splice(i, 1);
                                             i--;
+                                        } else {//private identifiers
+                                            if (/#[\w]+\s*}?/.test(diff[i].value)) {
+                                                diff.splice(i, 1);
+                                                i--;
+                                            }
                                         }
                                     }
                                 }
@@ -82,8 +106,8 @@ function generateJavaScriptFile(path, options) {
                         diff.forEach(function (part) {
                             // green for additions, red for deletions
                             // grey for common parts
-                            var color = part.added ? 'green' :
-                                part.removed ? 'red' : 'grey';
+                            var color = part.added ? 'green; font-weight: bolder; font-size: large' :
+                                part.removed ? 'red; font-weight: bolder; font-size: large' : 'grey';
                             span += `<span style="color:` + color + `">` + part.value + `</span>`;
                         });
                         var sourceText = `<pre>` + span + `</pre>`;
@@ -143,3 +167,5 @@ function cleanEmptyFoldersRecursively(folder) {
 generateJavaScriptFile(process.argv.slice(2));
 
 cleanEmptyFoldersRecursively(process.argv[3]);
+var hrend = process.hrtime(hrstart);
+console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
